6f659e511833cc76506483d67ac0ce741b960516
055-ABD-BASED <144401927+055-ABD-BASED@users.noreply.github.com>
2023-09-08 08:38:06 +0700

    Add files via upload

---
+++ program_konversi_suhu_based_055.py
@@ -0,0 +1,15 @@
+# Konversi_Suhu_Celsius_Ke_Fahrenhait
+
+celsius = int (input("masukkan nilai suhu c yang ingin anda konversi ke suhu f = "))
+conv_cel = 9*celsius/5+32
+
+print (f'hasil konversi {celsius} C menjadi {conv_cel}F')
+
+
+
+# konversi_Suhu_Fahrenhit_Ke_Celsius
+
+kelvin = int (input("masukkan nilai suhu K yang ingin anda konversi ke suhu c ="))
+konv_k = kelvin - 273.15
+
+print (f'hasil konversi {kelvin} K menjadi {konv_k} C')
